672e438b879a424e82b04e2afde0179eb0d20f54
Felipe Santos <felipesantosdealmeida.2020@gmail.com>
2021-03-23 22:43:11 -0300

    Create ex-InnerJoin-bdEstoque.sql

---
+++ Estudos/BDII/ex-InnerJoin-bdEstoque.sql
@@ -0,0 +1,21 @@
+USE bdEstoque
+
+-- a) Listar as descrições dos produtos ao lado do nome dos fabricantes;
+
+-- b) Listar as descrições dos produtos ao lado do nome dos fornecedores;
+
+-- c) Listar a soma das quantidades dos produtos agrupadas pelo nome do fabricante;
+
+-- d) Listar o total das vendas ao lado do nome do cliente;
+
+-- e) Listar a média dos preços dos produtos agrupados pelo nome do fornecedor;
+
+-- f) Listar todas a soma das vendas agrupadas pelo nome do cliente em ordem alfabética;
+
+-- g) Listar a soma dos preços dos produtos agrupados pelo nome do fabricante;
+
+-- h) Listar a média dos preços dos produtos agrupados pelo nome do fornecedor
+
+-- i) Listar a soma das vendas agrupadas pelo nome do produto
+
+-- j) Listar a soma das vendas pelo nome do cliente somente das vendas realizadas em fevereiro de 2014
