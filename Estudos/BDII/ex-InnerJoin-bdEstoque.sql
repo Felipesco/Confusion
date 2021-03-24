a91cf75b0984c0bc545a7eb485ab6657033085c8
Felipe Santos <felipesantosdealmeida.2020@gmail.com>
2021-03-24 10:36:46 -0300

    Início da atividade
    Comecei a fazer BD. Mais tarde faço o resto. :smile:

--- Estudos/BDII/ex-InnerJoin-bdEstoque.sql
+++ Estudos/BDII/ex-InnerJoin-bdEstoque.sql
@@ -1,10 +1,22 @@
 USE bdEstoque
 
 -- a) Listar as descrições dos produtos ao lado do nome dos fabricantes;
+SELECT descricaoProduto AS 'Produto', nomeFabricante AS 'Fabricante' FROM tbProduto
+	INNER JOIN tbFabricante
+		ON tbProduto.idFabricante = tbFabricante.idFabricante
+			ORDER BY nomeFabricante
 
 -- b) Listar as descrições dos produtos ao lado do nome dos fornecedores;
+SELECT descricaoProduto AS 'Produto', nomeFornecedor AS 'Fornecedor' FROM tbProduto
+	INNER JOIN tbFornecedor
+		ON tbProduto.idFornecedor = tbFornecedor.idFornecedor
+			ORDER BY nomeFornecedor
 
 -- c) Listar a soma das quantidades dos produtos agrupadas pelo nome do fabricante;
+SELECT COUNT(quantidadeProduto) AS 'Quant. Produtos', nomeFabricante AS 'Fabricante' FROM tbProduto
+	INNER JOIN tbFabricante
+		ON tbProduto.idFabricante = tbFabricante.idFabricante
+			GROUP BY nomeFabricante
 
 -- d) Listar o total das vendas ao lado do nome do cliente;
 
