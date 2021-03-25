9c4774c2b4c06acf175e8e864d82cd243736f5b2
Felipe Santos <felipesantosdealmeida.2020@gmail.com>
2021-03-24 22:18:19 -0300

    Atividade de "treino"
    Não terminei pois ainda estou com dúvida de como fazer os dois últimos.
    
    Fiz isso, mas né... Deu errado:
    
    '''
    SELECT SUM(valorTotalVenda) AS 'Soma das vendas', nomeProduto AS 'Produto' FROM tbVenda
    	INNER JOIN tbItensVenda ON tbVenda.idVenda = tbItensVenda.idProduto
    		INNER JOIN tbProduto ON tbItensVenda.idProduto = tbVenda.idPro
    '''
    
    Não entendi como fazer uma ligação com o produto :sweat_smile:

--- Estudos/BDII/ex-InnerJoin-bdEstoque.sql
+++ Estudos/BDII/ex-InnerJoin-bdEstoque.sql
@@ -19,15 +19,36 @@ SELECT COUNT(quantidadeProduto) AS 'Quant. Produtos', nomeFabricante AS 'Fabrica
 			GROUP BY nomeFabricante
 
 -- d) Listar o total das vendas ao lado do nome do cliente;
+SELECT COUNT(idVenda) AS 'Vendas', nomeCliente AS 'Cliente' FROM tbVenda
+	INNER JOIN tbCliente 
+		ON tbCliente.idCliente = tbVenda.idCliente
+			GROUP BY nomeCliente
 
 -- e) Listar a média dos preços dos produtos agrupados pelo nome do fornecedor;
+SELECT AVG(valorProduto) AS 'Preço Médio', nomeFornecedor AS 'Fornecedor' FROM tbProduto 
+	INNER JOIN tbFornecedor
+		ON tbFornecedor.idFornecedor = tbProduto.idFornecedor
+			GROUP BY nomeFornecedor
 
 -- f) Listar todas a soma das vendas agrupadas pelo nome do cliente em ordem alfabética;
+SELECT SUM(valorTotalVenda) AS 'Soma das vendas', nomeCliente AS 'Cliente' FROM tbVenda
+	INNER JOIN tbCliente
+		ON tbCliente.idCliente = tbVenda.idCliente
+			GROUP BY nomeCliente 
+				ORDER BY nomeCliente 
 
 -- g) Listar a soma dos preços dos produtos agrupados pelo nome do fabricante;
+SELECT SUM(valorProduto) AS 'Preço Produtos', nomeFabricante AS 'Fabricante' FROM tbProduto
+	INNER JOIN tbFabricante
+		ON tbFabricante.idFabricante = tbProduto.idFabricante
+			GROUP BY nomeFabricante
 
--- h) Listar a média dos preços dos produtos agrupados pelo nome do fornecedor
+-- h) Listar a média dos preços dos produtos agrupados pelo nome do fornecedor;
+SELECT AVG(valorProduto) AS 'Preço Médio', nomeFornecedor AS 'Fornecedor' FROM tbProduto
+	INNER JOIN tbFornecedor
+		ON tbFornecedor.idFornecedor = tbProduto.idFornecedor
+			GROUP BY nomeFornecedor
 
--- i) Listar a soma das vendas agrupadas pelo nome do produto
+-- i) Listar a soma das vendas agrupadas pelo nome do produto;
 
--- j) Listar a soma das vendas pelo nome do cliente somente das vendas realizadas em fevereiro de 2014
+-- j) Listar a soma das vendas pelo nome do cliente somente das vendas realizadas em fevereiro de 2014.
