8de32e5cad99ed39a589ca0aa3b1a6daa3be33ee
Felipe Santos <felipesantosdealmeida.2020@gmail.com>
2021-04-07 15:06:35 -0300

    Update bdEscolaIdiomas.sql

--- Estudos/BDII/bdEscolaIdiomas.sql
+++ Estudos/BDII/bdEscolaIdiomas.sql
@@ -12,6 +12,7 @@ CREATE TABLE tbCurso(
 
 CREATE TABLE tbAluno(
 	codAluno INT PRIMARY KEY IDENTITY (1,1)
+	, nomeAluno VARCHAR(50) NOT NULL
 	, dataNascAluno SMALLDATETIME NOT NULL
 	, rgAluno VARCHAR(12) NOT NULL
 	, naturalidadeAluno VARCHAR(2) NOT NULL
@@ -31,3 +32,60 @@ CREATE TABLE tbMatricula(
 	, codTurma INT FOREIGN KEY REFERENCES tbTurma(codTurma)
 )
 
+-- Inserção de Dados
+INSERT INTO tbCurso(nomeCurso, cargaHoraria, valorCurso)
+VALUES ('Inglês', 2000, 356.00)
+	, ('Alemão', 3000, 478.00)
+	, ('Espanhol', 4000, 500.00)
+
+INSERT INTO tbAluno(nomeAluno, dataNascAluno, rgAluno, naturalidadeAluno)
+VALUES ('Paulo Santos', '10/03/2000', '82.282.122-0', 'SP')
+	, ('Maria da Gloria', '03/10/1999', '45.233.123-0', 'SP')
+	, ('Pedro Nogueira da Silva', '04/04/1998', '23.533.211-9', 'SP')
+	, ('Gilson Barros Silva', '09/09/1995', '34.221.111-x', 'PE')
+	, ('Mariana Barbosa Santos', '10/10/2001', '54.222.122-9', 'RJ') 
+	, ('Alessandro Pereira', '11/10/2003', '24.402.454-9', 'ES')
+	, ('Aline Melo', '08/10/2001', '88.365.012-3', 'RJ')
+
+INSERT INTO tbTurma(nomeTurma, codCurso, horarioTurma)
+VALUES ('1|A', 1, '12:00:00')
+	, ('1|C', 3, '18:00:00')
+	, ('1|B', 1, '18:00:00')
+	, ('1AA', 2, '19:00:00')
+
+INSERT INTO tbMatricula(dataMatricula, codAluno, codTurma)
+VALUES ('03/10/2015', 1, 1)
+	, ('04/05/2014', 2, 1)
+	, ('04/05/2014', 2, 4)
+	, ('03/05/2012', 3, 2)
+	, ('03/03/2016', 1, 3)
+	, ('07/05/2015', 4, 2)
+	, ('05/07/2015', 4, 3)
+
+
+-- Atividade
+--1)Apresentar os nomes dos alunos ao lado do nome dos cursos que eles fazem;
+
+--2)Apresentar a quantidade de alunos matriculados por nome do curso;
+
+--3)Apresentar a quantidade de alunos matriculados por nome da turma;
+
+--4)Apresentar a quantidade de alunos que fizeram matricula em maio de 2016;
+
+--5)Apresentar o nome dos alunos em ordem alfabética ao lado do nome das turmas e os nomes dos cursos em que estão matriculados;
+
+--6)Apresentar o nome dos cursos e os horários em que eles são oferecidos;
+
+--7)Apresentar a quantidade de alunos nascidos por estado que estejam matriculados no curso de ingles;
+
+--8)Apresentar o nome dos alunos ao lado da data de matrícula no formato dd/mm/aaaa;
+
+--9)Apresentar os alunos cujo nome comece com A e que estejam matriculados no curso de inglês;
+
+--10)Apresentar a quantidade de matriculas feitas no ano de 2016;
+
+--11)Apresentar a quantidade de matriculas por nome do curso;
+
+--12)Apresentar a quantidade de alunos que fazem os cursos que custam mais de R$300,00;
+
+--13)Apresentar os nomes dos alunos que fazem o curso de alemão.
